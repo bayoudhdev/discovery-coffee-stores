26bf909d82e36bb95848b9055aed7b0df4c0d8e0
Mohamed BAYOUDH <mohamed.bayoudh@socrate.education>
2023-01-21 04:20:33 +0100

    feat : adding config for netlify

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: false,
+    target : "serverless",
     images: {
         domains: ['images.unsplash.com']
-    }
+    },
+
 }
 
 module.exports = nextConfig
